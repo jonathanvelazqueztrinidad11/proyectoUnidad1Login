b54221e265a0afdc2bde2ea38e047a41de5025a7
jonathanvelazqueztrinidad11 <jonavt11@gmail.com>
2020-02-11 08:51:38 -0600

    inicio de login

---
+++ login.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+
+    <form action="login.php" method="post">
+      <input type="text" name="email" placeholder="ingrese su email">
+      <input type="password" name="password" placeholder="ingresa tu contraseña">
+      <input type="submit" value="send">
+
+    </form>
+
+  </body>
+</html>
