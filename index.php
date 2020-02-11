c570f8c8d8f2767509e894cf9a3ce6f6ffebe9cc
jonathanvelazqueztrinidad11 <jonavt11@gmail.com>
2020-02-11 00:37:17 -0600

    bienvenido

--- index.php
+++ index.php
@@ -1,3 +1,10 @@
-<?php
-echo "hola mundo";
-?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>bienvenido a mi aplicacion</title>
+  </head>
+  <body>
+
+  </body>
+</html>
